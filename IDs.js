98f86eeca395465da76a41faa1b8707c8c991b5b
MC-Stephan <112822300+MC-Stephan@users.noreply.github.com>
2022-09-04 21:15:18 +0200

    Update IDs.js

--- IDs.js
+++ IDs.js
@@ -1880,5 +1880,5 @@ var locdata =
 if(window.location.search){
   const params = new URLSearchParams(window.location.search);
  if (params.has('ort')){
-  $('.ort').text("in" + locdata[params.get('ort')]);
+  $('.ort').text("in " + locdata[params.get('ort')]);
  }}
